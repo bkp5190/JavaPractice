e4218cf2387b6ea9aeda9f08abf37aefb4e4df34
Boney Patel <boneypatel37@yahoo.com>
2023-07-02 17:03:26 -0400

    Primitive Types

--- MyFirstJavaProject/src/App.java
+++ MyFirstJavaProject/src/App.java
@@ -1,11 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
         // (type) (identifier) (assignment operator) (value)
-        byte age = 30;
-        age = 35;
-
+        
         // Primitive Types
-
+        
         // |     Type     | Bytes |      Range
         // |     byte     |   1   |   [-128, 127]
         // |     short    |   2   |   [-32K, 32k]    
@@ -15,8 +13,20 @@ public class App {
         // |     double   |   8   |
         // |     char     |   1   |    A, B, C
         // |     boolean  |   1   |   true/false
+        
+        // camelCase for variable names and meaningful names
+        
+        byte age = 30;
+        long viewsCount = 3_123_456_789L;
+        float price = 10.99F;
+        char letter = 'A';
+        boolean isEligible = false;
+        age = 35;
 
-
+        System.out.println(viewsCount);
         System.out.println(age);
+        System.out.println(price);
+        System.out.println(letter);
+        System.out.println(isEligible);
     }
 }
