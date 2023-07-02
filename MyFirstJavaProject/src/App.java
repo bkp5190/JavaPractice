94389ba15b911d069a54e799eabe3196f1dc3aca
Boney Patel <boneypatel37@yahoo.com>
2023-07-02 16:33:22 -0400

    initial commit

---
+++ MyFirstJavaProject/src/App.java
@@ -0,0 +1,22 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // (type) (identifier) (assignment operator) (value)
+        byte age = 30;
+        age = 35;
+
+        // Primitive Types
+
+        // |     Type     | Bytes |      Range
+        // |     byte     |   1   |   [-128, 127]
+        // |     short    |   2   |   [-32K, 32k]    
+        // |     int      |   4   |    [-2B, 2B]
+        // |     long     |   8   |
+        // |     float    |   4   |
+        // |     double   |   8   |
+        // |     char     |   1   |    A, B, C
+        // |     boolean  |   1   |   true/false
+
+
+        System.out.println(age);
+    }
+}
